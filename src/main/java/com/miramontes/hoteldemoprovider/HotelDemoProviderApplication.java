660a02e8ef4f8ad764ea928a589a41b325e1a8bc
Jesus Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2023-03-14 16:43:47 -0600

    init commit

---
+++ src/main/java/com/miramontes/hoteldemoprovider/HotelDemoProviderApplication.java
@@ -0,0 +1,12 @@
+package com.miramontes.hoteldemoprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelDemoProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelDemoProviderApplication.class, args);
+    }
+}
---
+++ src/test/java/com/miramontes/hoteldemoprovider/HotelDemoProviderApplicationTests.java
@@ -0,0 +1,11 @@
+package com.miramontes.hoteldemoprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelDemoProviderApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
