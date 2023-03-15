322968354c1e653b88588d7c80bc6e31b14ebad1
Jesus Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2023-03-15 10:49:10 -0600

    Method to convert from generated class to model.

--- src/main/java/com/miramontes/hoteldemoprovider/util/HotelUtil.java
+++ src/main/java/com/miramontes/hoteldemoprovider/util/HotelUtil.java
@@ -9,9 +9,19 @@ public class HotelUtil {
 
     public static Hotel convertModelToWs(HotelModel model) {
         Hotel hotel = new Hotel();
+        hotel.setId(model.getId());
         hotel.setAddress(model.getAddress());
         hotel.setName(model.getName());
         hotel.setRating(model.getRating());
         return hotel;
+    }
+
+    public static HotelModel convertWsToModel(Hotel ws) {
+        HotelModel model = new HotelModel();
+        model.setId(ws.getId());
+        model.setAddress(ws.getAddress());
+        model.setName(ws.getName());
+        model.setRating(ws.getRating());
+        return model;
     }
 }
